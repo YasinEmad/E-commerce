97a842e947b08b1073634cec157385b8293f34f8
Yasin Emad <yemad7676@gmail.com>
2025-03-17 03:29:31 +0200

    save data with redux psist

--- src/components/Redux/store.jsx
+++ src/components/Redux/store.jsx
@@ -1,18 +1,42 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // يستخدم localStorage
 import productsReducer from "./productsSlice";
 import cartReducer from "./cartSlice";
 import languageReducer from "./languageSlice";
 import categoryReducer from "./categorySlice";
-import ordersReducer from "./ordersSlice"; // Import the ordersSlice
+import ordersReducer from "./ordersSlice"; // استيراد ordersSlice
+import { combineReducers } from "redux";
 
-const store = configureStore({
-  reducer: {
-    products: productsReducer,
-    cart: cartReducer,
-    language: languageReducer,
-    category: categoryReducer,
-    orders: ordersReducer, // Add the orders reducer
-  },
+// إعداد حفظ البيانات (persist)
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart", "orders"], // حدد الـ reducers التي تريد حفظها
+};
+
+// دمج جميع الـ reducers
+const rootReducer = combineReducers({
+  products: productsReducer,
+  cart: cartReducer,
+  language: languageReducer,
+  category: categoryReducer,
+  orders: ordersReducer,
 });
 
-export default store;
+// إنشاء persist reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// إنشاء store
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // تجنب مشاكل القيم غير القابلة للتسلسل
+    }),
+});
+
+// إعداد persistor لحفظ البيانات
+const persistor = persistStore(store);
+
+export { store, persistor };
--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from './components/Redux/store.jsx'; 
+import { PersistGate } from "redux-persist/integration/react"; // استيراد PersistGate
+import { store, persistor } from "./components/Redux/store.jsx"; // استيراد persistor
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <StrictMode>
-        <App />
-      </StrictMode>
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <StrictMode>
+          <App />
+        </StrictMode>
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
